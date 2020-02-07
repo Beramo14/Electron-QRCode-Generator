7593469d8185e3494ac59fcdc805501d8ff82571
Beramo <aqaq556@naver.com>
2020-02-07 18:05:04 +0900

    레이아웃 생성

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ let mainWindow;
 
 function createWindow(){
     mainWindow = new BrowserWindow({
-        width: 300,
-        height: 400
+        width: 600,
+        height: 400,
+        resizable: false
     });
 
+    mainWindow.loadURL(`file://${__dirname}/static/index.html`);
     mainWindow.on('closed', () =>{
         mainWindow = null;
     });
+    mainWindow.openDevTools();
+    mainWindow.removeMenu();
 }
 
 app.on('ready', () =>{
