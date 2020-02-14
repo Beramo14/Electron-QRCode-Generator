ac53ee373b7c9137c6dbd20e9ef8c184bf988b7e
Beramo <aqaq556@naver.com>
2020-02-14 17:52:25 +0900

    QR코드 생성 및 display 테스트

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-const{app, BrowserWindow} = require('electron');
-
+const{app, BrowserWindow, ipcMain} = require('electron');
+const qrcode = require('qrcode');
+const path = require("path");
+const fs = require("fs");
 let mainWindow;
 
 function createWindow(){
     mainWindow = new BrowserWindow({
-        width: 600,
+        width: 650,
         height: 400,
-        resizable: false
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: false,
+            preload: path.join(__dirname, "preload.js")
+        }
     });
 
-    mainWindow.loadURL(`file://${__dirname}/static/index.html`);
+    console.log(path.join(__dirname, "preload.js"));
+
+    // mainWindow.loadURL(`file://${__dirname}/static/index.html`);
+    mainWindow.loadFile(path.join(__dirname, "static/index.html"));
     mainWindow.on('closed', () =>{
         mainWindow = null;
     });
     mainWindow.openDevTools();
     mainWindow.removeMenu();
+
+    ipcMainEventHandler();
+
 }
 
 app.on('ready', () =>{
     createWindow();
 });
+
+app.allowRendererProcessReuse = true;
+
+function ipcMainEventHandler(){
+    var qrOption = {
+        errorCorrectionLevel:'L',
+        type: 'image/png',
+        scale: 19,
+        margin: 5
+    }
+
+    ipcMain.on('message-test',(event, arg) => {
+        console.log(arg);
+
+        qrcode.toDataURL(arg, qrOption, function(err, arg){
+            event.sender.send("message-test", arg);
+        });
+
+    });
+
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
-console.log("log");
+
+document.querySelector("#test-button").addEventListener('click', () => {
+    ipcRenderer.send("message-test","https://species.nibr.go.kr/digital/mobile/viewSpecies.do?ktsn=120000042647");
+});
+
+ipcRenderer.on("message-test", (event,arg) => {
+    var qrImg = document.querySelector("#qr-img");
+    qrImg.setAttribute("src",arg);
+    qrImg.removeAttribute("style");
+});
