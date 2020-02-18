410e3a03a3f8f39fbd829f616bbbe7e752f9db7e
Beramo <aqaq556@naver.com>
2020-02-18 18:30:45 +0900

    입력한 값이 있을경우에만 QR코드 생성

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,13 @@
 
 document.querySelector("#QRCode-generate-button").addEventListener('click', () => {
     var qrcodeTarget = document.querySelector("#url-input").value;
-    ipcRenderer.send("QRCodeCh",qrcodeTarget);
+    if(qrcodeTarget){
+        ipcRenderer.send("QRCodeCh", qrcodeTarget);
+        document.querySelector('#thisQrText').value = qrcodeTarget;
+    }
 });
 
-ipcRenderer.on("QRCodeCh", (event,arg) => {
+ipcRenderer.on("QRCodeCh", (event, arg) => {
     var qrImg = document.querySelector("#qr-img");
     qrImg.setAttribute("src",arg);
     qrImg.removeAttribute("style");
