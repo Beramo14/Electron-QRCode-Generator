7644b078fce3e4b93a2141d65e350c97ef55bb62
Beramo <aqaq556@naver.com>
2020-02-19 10:38:30 +0900

    해당 이미지 파일로 저장하려는 텍스트 node로 전송하는 로직 추가 해당 이미지 파일로 저장하려는 텍스트 node로 전송하는 로직 추가 각각 이벤트 마다 주석추가

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-
+/*---------- QRCode 단일 생성 ----------*/
 document.querySelector("#QRCode-generate-button").addEventListener('click', () => {
     var qrcodeTarget = document.querySelector("#url-input").value;
     if(qrcodeTarget){
@@ -7,8 +7,18 @@ document.querySelector("#QRCode-generate-button").addEventListener('click', () =
     }
 });
 
+/*---------- 현재 QRCode 이미지파일로 저장 ----------*/
+document.querySelector('#qr-image-save').addEventListener('click', () =>{
+    var qrcodeTarget = document.querySelector("#thisQrText").value;
+    if(qrcodeTarget){
+        ipcRenderer.send("QRCodeSaveCh", qrcodeTarget);
+    }
+});
+
+
+/*---------- QR 이미지 출력 ----------*/
 ipcRenderer.on("QRCodeCh", (event, arg) => {
     var qrImg = document.querySelector("#qr-img");
-    qrImg.setAttribute("src",arg);
+    qrImg.setAttribute("src", arg);
     qrImg.removeAttribute("style");
 });
